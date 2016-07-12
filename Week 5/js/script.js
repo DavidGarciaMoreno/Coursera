ae5ed2e1902662b076b14037c7b014879734e1aa
David Garcia <DavidG@embarcadero.com>
2016-07-12 14:17:58 +0200

    fixed menu item selected

--- Week 5/js/script.js
+++ Week 5/js/script.js
@@ -40,6 +40,21 @@ $(function () {  // Same as document.addEventListener("DOMContentLoaded"...)
 		return string;
 	}
 
+	// Remove the class 'active' from home and switch to Menu button
+	var switchMenuToActive = function () {
+		// Remove 'active' from home button
+		var classes = document.querySelector("#navHomeButton").className;
+		classes = classes.replace(new RegExp("active", "g"), "");
+		document.querySelector("#navHomeButton").className = classes;
+
+		// Add 'active' to menu button if not already there
+		classes = document.querySelector("#navMenuButton").className;
+		if (classes.indexOf("active") == -1) {
+			classes += " active";
+			document.querySelector("#navMenuButton").classes = classes;
+		}
+	};
+
 	// On page load (before images or CSS)
 	document.addEventListener("DOMContentLoaded", function (event) {
 	  showLoading("#main-content");
@@ -78,6 +93,9 @@ $(function () {  // Same as document.addEventListener("DOMContentLoaded"...)
 				$ajaxUtils.sendGetRequest(
 					categoryHtml,
 					function (categoryHtml) {
+	          // Switch CSS class active to menu button
+  	        switchMenuToActive();						
+  	        
 						var categoriesViewHtml = buildCategoriesViewHtml(
 							categories,
 							categoriesTitleHtml,
