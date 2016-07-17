f3278ceaa82df578200980825cb2ea7a9050565c
David García Moreno <davigarsia@gmail.com>
2016-07-17 22:39:22 +0200

    Mi primer juego unity

---
+++ DesarrolloVideojuegosUnity/Week 1/MyFirstGame/Assets/MyAssets/EnemyControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControl : MonoBehaviour {
+  public float velocity = -1f;
+  Rigidbody2D rigitBody;
+
+	// Use this for initialization
+	void Start () {
+    rigitBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+    Vector2 v = new Vector2(velocity, 0);
+    rigitBody.velocity = v;
+	}
+}
